02254c25ed6183498c132aa0c1df10cfc36afe06
ZGQ Inc. <zgqinc@gmail.com>
2024-09-02 17:21:18 +0800

    11

---
+++ custom-app.ps1
@@ -0,0 +1,21 @@
+$apiUrl = "https://api.github.com/repos/harbassan/spicetify-apps/releases"
+$downloadPath = "$env:APPDATA\spicetify\CustomApps"
+
+$response = Invoke-RestMethod -Uri $apiUrl
+$downloadUrl = $response.assets | ForEach-Object { $_.browser_download_url } | Where-Object { $_ -match "stats.*\.zip$" } | Select-Object -First 1
+
+if (-not $downloadUrl) {
+    Write-Error "stats release not found."
+    exit 1
+}
+
+$tempZipPath = "$env:TEMP\spicetify-stats.zip"
+
+Invoke-WebRequest -Uri $downloadUrl -OutFile $tempZipPath
+Expand-Archive -Path $tempZipPath -DestinationPath $downloadPath -Force
+Remove-Item -Path $tempZipPath
+Write-Output "success mv stats custom app."
+
+spicetify config custom_apps stats
+spicetify config custom_apps lyrics-plus
+spicetify apply
