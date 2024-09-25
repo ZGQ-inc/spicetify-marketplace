1a6a5610f73844afeda7f7fa067183cc71fabf46
ZGQ Inc. <zgqinc@gmail.com>
2024-09-25 19:27:50 +0800

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,17 +1,8 @@
 # https://t.me/ZGQinc
 
 $spotxCommand = "iex ""& { $(iwr -useb 'https://raw.githubusercontent.com/SpotX-Official/spotx-official.github.io/main/run.ps1') } -new_theme"""
-$spotxInput = @"
-N
-Y
-"@
-$spotxProcess = Start-Process powershell -ArgumentList "-NoProfile", "-Command", $spotxCommand -RedirectStandardInput "spotxInput.txt" -NoNewWindow -Wait
 
 $spicetifyCommand = "iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex"
-$spicetifyInput = @"
-Y
-"@
-$spicetifyProcess = Start-Process powershell -ArgumentList "-NoProfile", "-Command", $spicetifyCommand -RedirectStandardInput "spicetifyInput.txt" -NoNewWindow -Wait
 
 $apiUrl = "https://api.github.com/repos/harbassan/spicetify-apps/releases"
 $downloadPath = "$env:APPDATA\spicetify\CustomApps"
