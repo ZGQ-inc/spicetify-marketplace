3be2dc236d3c44cf4bdbeec3b25616c28b2a4510
ZGQ Inc. <zgqinc@gmail.com>
2024-09-25 21:44:26 +0800

    update2

--- install.ps1
+++ install.ps1
@@ -1,8 +1,14 @@
 # https://t.me/ZGQinc
 
+Write-Output "Installing Spotify modfied by SpotX..."
+
 iex ""& { $(iwr -useb 'https://raw.githubusercontent.com/SpotX-Official/spotx-official.github.io/main/run.ps1') } -new_theme""
 
+Write-Output "Installing Spicetify..."
+
 iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex
+
+Write-Output "Installing custom-apps and extensions..."
 
 $apiUrl = "https://api.github.com/repos/harbassan/spicetify-apps/releases"
 $downloadPath = "$env:APPDATA\spicetify\CustomApps"
@@ -11,8 +17,7 @@ $response = Invoke-RestMethod -Uri $apiUrl
 $downloadUrl = $response.assets | ForEach-Object { $_.browser_download_url } | Where-Object { $_ -match "stats.*\.zip$" } | Select-Object -First 1
 
 if (-not $downloadUrl) {
-    Write-Error "stats release not found."
-    exit 1
+    Write-Error "Error. stats release not found."
 }
 
 $tempZipPath = "$env:TEMP\spicetify-stats.zip"
@@ -20,7 +25,6 @@ $tempZipPath = "$env:TEMP\spicetify-stats.zip"
 Invoke-WebRequest -Uri $downloadUrl -OutFile $tempZipPath
 Expand-Archive -Path $tempZipPath -DestinationPath $downloadPath -Force
 Remove-Item -Path $tempZipPath
-Write-Output "success mv stats custom app."
 
 spicetify config custom_apps stats
 spicetify config custom_apps lyrics-plus
