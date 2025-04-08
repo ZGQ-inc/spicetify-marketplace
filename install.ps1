7b8b589c77781a4c81bc1474dc047f3bd7230e45
ZGQ Inc. <zgqinc@gmail.com>
2025-04-08 22:10:47 +0800

    rollback & back to all English

--- install.ps1
+++ install.ps1
@@ -1,6 +1,67 @@
 # https://t.me/ZGQinc
-$url = "https://spicetify.zgqinc.gq/run.exe"
-$output = Join-Path $env:TEMP "install_spotify.exe"
-Invoke-WebRequest -Uri $url -OutFile $output
-Start-Process -FilePath $output -Wait
-Remove-Item -Path $output -Force
+
+# $url = "https://spicetify.zgqinc.gq/run.exe"
+# $output = Join-Path $env:TEMP "install_spotify.exe"
+# Invoke-WebRequest -Uri $url -OutFile $output
+# Start-Process -FilePath $output -Wait
+# # Remove-Item -Path $output -Force
+# src of install.exe, convert to exe with ps2exe
+# https://github.com/MScholtes/PS2EXE
+
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$ProgressPreference = 'SilentlyContinue'
+
+Write-Output "Installing Spotify and patching with SpotX..."
+iex "& { $(iwr -useb 'https://raw.githubusercontent.com/SpotX-Official/spotx-official.github.io/main/run.ps1') } -confirm_uninstall_ms_spoti -sp-over -new_theme -block_update_on -podcasts_on -dev -exp_spotify -adsections_off -cl 20000 -topsearchbar -newFullscreenMode -canvasHome -rightsidebarcolor -hide_col_icon_off"
+
+Write-Output "Installing Spicetify..."
+iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex
+
+Write-Output "Installing custom-apps and extensions..."
+
+Write-Output "Downloading Enhancify..."
+# iwr -useb https://github.com/ECE49595-Team-6/EnhancifyInstall/releases/latest/download/install.ps1 | iex
+$apiUrl = "https://api.github.com/repos/ECE49595-Team-6/EnhancifyInstall/releases/latest"
+$downloadPath = "$env:APPDATA\spicetify\CustomApps\Enhancify"
+$response = Invoke-RestMethod -Uri $apiUrl
+$downloadUrl = $response.assets[0].browser_download_url
+if (-not $downloadUrl) {
+    Write-Error "Error: Failed to find download link for Enhancify release."
+    exit
+}
+$tempZipPath = "$env:TEMP\Enhancify.zip"
+Invoke-WebRequest -Uri $downloadUrl -OutFile $tempZipPath
+New-Item -ItemType Directory -Path $downloadPath -Force | Out-Null
+Expand-Archive -Path $tempZipPath -DestinationPath $downloadPath -Force
+Remove-Item -Path $tempZipPath
+
+Write-Output "Downloading stats..."
+$apiUrl = "https://api.github.com/repos/harbassan/spicetify-apps/releases"
+$downloadPath = "$env:APPDATA\spicetify\CustomApps"
+$response = Invoke-RestMethod -Uri $apiUrl -UseBasicParsing
+$downloadUrl = $response.assets | ForEach-Object { $_.browser_download_url } | Where-Object { $_ -match "stats.*\.zip$" } | Select-Object -First 1
+if (-not $downloadUrl) {
+    Write-Error "Error: Failed to find download link for stats release."
+    exit
+}
+$tempZipPath = "$env:TEMP\spicetify-stats.zip"
+Invoke-WebRequest -Uri $downloadUrl -OutFile $tempZipPath -UseBasicParsing
+Expand-Archive -Path $tempZipPath -DestinationPath $downloadPath -Force
+Remove-Item -Path $tempZipPath
+
+Write-Output "Configuring Spicetify..."
+spicetify config custom_apps Enhancify
+spicetify config custom_apps stats
+spicetify config custom_apps lyrics-plus
+spicetify config extensions bookmark.js
+spicetify config extensions fullAppDisplay.js
+spicetify config extensions keyboardShortcut.js
+spicetify config extensions loopyLoop.js
+spicetify config extensions popupLyrics.js
+spicetify config extensions shuffle+.js
+spicetify config extensions trashbin.js
+spicetify config extensions webnowplaying.js
+spicetify config sidebar_config 0
+spicetify apply
+
+Write-Output "Installation completed."
