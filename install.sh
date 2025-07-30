73a075dbd737c9d7b21d1ecbf796caf4a611520b
ZGQ Inc. <zgqinc@gmail.com>
2025-07-30 20:06:40 +0800

    fix

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # https://t.me/ZGQinc
 
-set -e
-
 echo "添加 Spotify 软件源..."
 curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
 echo "deb https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list > /dev/null
@@ -101,4 +99,4 @@ spicetify apply
 
 rm -rf "$TMP_DIR"
 
-echo -e "\n安装完成。"
+echo "\n安装完成。"
